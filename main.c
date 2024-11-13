badfe5cbcbe894d50e839361e5030cf43260f5bb
Ajay-AB-nitc <ajay_b240124cs@nitc.ac.in>
2024-11-13 18:03:56 +0530

    fixed read and added print for s_graph

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ typedef struct {
 }s_graph ;  
 
 
+void read_s_graph(s_graph * s);
+void print_s_graph(s_graph *s);
+
 int main(){
 
   s_graph wheel_4; 
@@ -19,6 +22,10 @@ int main(){
   memcpy(wheel_4.vertices, wheel_4_v_init, sizeof(wheel_4_v_init));
   memcpy(wheel_4.edges, wheel_4_e_init, sizeof(wheel_4_e_init));
 
+  s_graph wheel_5;
+  read_s_graph(&wheel_5);
+  print_s_graph(&wheel_5);
+  
   return 0;   
 }
 
@@ -29,14 +36,14 @@ void read_s_graph(s_graph * s){
 
   
   int num_edges;
-  printf("Enter the number of vertices: ");
+  printf("Enter the number of edges: ");
   scanf("%d", &num_edges);
 
   s -> n_vertices =   num_vertices;
   for (int i = 0; i < num_vertices; i++){
     char inp;
     printf("Enter a vertex: ");
-    scanf("%c", &inp);
+    scanf(" %c", &inp);
     s -> vertices[i] = inp;
   }
   
@@ -44,10 +51,26 @@ void read_s_graph(s_graph * s){
   for (int i = 0; i < num_edges; i++){
     char inp1, inp2;
     printf("Enter the edge (start, end)\n");
-    scanf("%c %c", &inp1 , &inp2);
+    scanf(" %c %c", &inp1 , &inp2);
     s -> edges[i][0] = inp1;
     s -> edges[i][1] = inp2;
   }
 
+}
+
+void print_s_graph(s_graph *s){
+  int n_vertices = s->n_vertices;
+  int n_edges= s->n_edges;
+  printf("No. of vertices: %d \n", n_vertices);
+  printf("No. of edges: %d \n", n_edges);
+
+  printf("Vertices:");
+  for (int i = 0; i < n_vertices; i++){
+    printf("%c ", s->vertices[i]);
+  }
+  printf("\nEdges:\n");
+  for (int i = 0; i < n_edges; i++){
+    printf("%c %c\n", s->edges[i][0],s->edges[i][1]);
+  }
 }
 
