4a88aed4501ba2992202273f0fc607512985bd36
Ajay-AB-nitc <ajay_b240124cs@nitc.ac.in>
2024-11-29 22:25:13 +0530

    updated main.c

--- main.c
+++ main.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
 #include "simple_undirected.h"
+#include "visuals.c"
 
 
-int main(){
-  s_graph c5;
+int main(void)
+{ 
+  int val = 0;
+  int hovered_vertex = 0;
+  int n_graphs = load_su_graph();
+  InitWindow(1910, 1070, "raylib [core] example - basic window");
+  int x_dist = 20;
+  int y_dist = 20;
 
-  read_s_graph(&c5);
-  print_s_graph(&c5);
+  while (!WindowShouldClose())
+  {
+    Vector2 mouse_pos = GetMousePosition();
+    BeginDrawing();
+
+    ClearBackground(WHITE);
+
+   
+    int inp = GetKeyPressed();
+    if (inp != 0 && inp > 47 && inp < 58) val = inp - 48;
+    if (inp == 68 && val < n_graphs -1) val++;
+    if (inp == 65 && val > 0) val --;
+    DrawText(su_arr[val].name, 10, 40, 60, BLACK);
+    draw_su_graph(&su_arr[val]);
+
+
+   for (int i = 0; i < su_arr[val].n_vertices; i++) {
+    if (is_hovered(&su_arr[val].vertices[i], mouse_pos, 30.0)){
+        highlight(&su_arr[val].vertices[i], i, BLACK);
+        highlight_neighbours(&su_arr[val].vertices[i], &su_arr[val]);
+      }
+
+    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && is_hovered(&su_arr[val].vertices[i], mouse_pos, 30.0))
+    {
+      hovered_vertex = i; 
+      x_dist = mouse_pos.x - su_arr[val].vertices[i].x;
+      y_dist = mouse_pos.y - su_arr[val].vertices[i].y;
+    }
+
+   }   
+    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT) && is_hovered(&su_arr[val].vertices[hovered_vertex], mouse_pos, 500.0)){
+      su_arr[val].vertices[hovered_vertex].x = mouse_pos.x - x_dist;
+      su_arr[val].vertices[hovered_vertex].y = mouse_pos.y - y_dist;
+
+    }
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
-
-
-
-
-
 
 
--- visuals.c
+++ visuals.c
@@ -11,59 +11,6 @@ int is_hovered(vertex *, Vector2 mouse_pos, float radius);
 void highlight(vertex * v, int n,  Color color);
 void highlight_neighbours(vertex * v, su_graph * s);
 
-int main(void)
-{ 
-  int val = 0;
-  int hovered_vertex = 0;
-  int n_graphs = load_su_graph();
-  InitWindow(1910, 1070, "raylib [core] example - basic window");
-  int x_dist = 20;
-  int y_dist = 20;
-
-  while (!WindowShouldClose())
-  {
-    Vector2 mouse_pos = GetMousePosition();
-    BeginDrawing();
-
-    ClearBackground(WHITE);
-
-   
-    int inp = GetKeyPressed();
-    if (inp != 0 && inp > 47 && inp < 58) val = inp - 48;
-    if (inp == 68 && val < n_graphs -1) val++;
-    if (inp == 65 && val > 0) val --;
-    DrawText(su_arr[val].name, 10, 40, 60, BLACK);
-    draw_su_graph(&su_arr[val]);
-
-
-   for (int i = 0; i < su_arr[val].n_vertices; i++) {
-    if (is_hovered(&su_arr[val].vertices[i], mouse_pos, 30.0)){
-        highlight(&su_arr[val].vertices[i], i, BLACK);
-        highlight_neighbours(&su_arr[val].vertices[i], &su_arr[val]);
-      }
-
-    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && is_hovered(&su_arr[val].vertices[i], mouse_pos, 30.0))
-    {
-      /*printf("%d\n", is_hovered(&su_arr[val].vertices[0], mouse_pos));*/
-      hovered_vertex = i; 
-      x_dist = mouse_pos.x - su_arr[val].vertices[i].x;
-      y_dist = mouse_pos.y - su_arr[val].vertices[i].y;
-      /*printf("%s\n") */
-    }
-
-   }   
-    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT) && is_hovered(&su_arr[val].vertices[hovered_vertex], mouse_pos, 500.0)){
-      su_arr[val].vertices[hovered_vertex].x = mouse_pos.x - x_dist;
-      su_arr[val].vertices[hovered_vertex].y = mouse_pos.y - y_dist;
-
-    }
-    EndDrawing();
-  }
-
-  CloseWindow();
-  return 0;
-}
-
 
 void draw_su_vertex(vertex* pvertex, int n)
 {
